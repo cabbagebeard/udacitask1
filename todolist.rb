de94985616aa2ea30e847f1ae4eee99255e26cb1
Kevin Windisch <kwindisch1992@gmail.com>
2016-01-25 21:22:53 -0600

    Added functionality for adding new items, listing the items, setting the items to complete, and checking whether or not an item is complete

--- todolist.rb
+++ todolist.rb
@@ -1,7 +1,66 @@
 class TodoList
-    # methods and stuff go here
+
+	attr_reader :title, :items
+
+    def initialize(list_title)
+    	@title = list_title
+    	@items = Array.new
+    end
+
+    def rename(list_title)
+    	@title = list_title
+    end
+
+    def add_item(new_item)
+        item = Item.new(new_item)
+        @items.push(item)
+
+    end
+
+    def list
+    	line_break = "=" * 10
+
+    	puts @title
+    	puts line_break
+    	@items.each { |item| item.print}
+    	puts line_break
+    	puts @items.length
+	end
+    
 end
 
 class Item
-    # methods and stuff go here
+
+	attr_reader :task, :complete
+
+    def initialize(task)
+    	@task = task
+    	@complete = false
+    end
+
+    def complete
+    	#NOT CURRENTLY FUNCTIONAL
+    	@complete = true
+    	puts "Would you like to remove item from your list? Y/N"
+    	answer = gets.chomp.to_s.upcase!
+
+    	if answer == "Y"
+    		@items.delete(task)
+    		puts "Item deleted!"
+    	elsif answer == "N"
+    		puts "Item not deleted"
+    	else 
+    		puts "Please put Y or N and try again"   		
+    	end
+    end
+
+    def is_complete?
+    	@complete
+    end
+
+    def print
+    	puts "#{@task}   Completed:#{@complete}"
+    end
+
 end
+
